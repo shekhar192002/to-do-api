7d9e2812b7d3da1b37788be2b4846eeb06c01aca
sgoswa2 <sgoswa2@asda.co.uk>
2019-02-19 16:01:00 +0000

    MLAB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/ToDoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ToDoApp');
 
 module.exports = {mongoose};
